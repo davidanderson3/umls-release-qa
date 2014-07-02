ae18bab6f7de0ccb8838a4f9b4d5285f614651ef
kundud <kundud@52887518-325f-0410-8f0e-f4756d5e9a5d>
2014-07-02 16:48:19 +0000

    update- added lang3 jar
    git-svn-id: https://svn.nlm.nih.gov/apps/umls2/umlsdoc/trunk@14915 52887518-325f-0410-8f0e-f4756d5e9a5d

--- umls.nlm.nih.gov/UtsSourceDocumentation/src/gov/nih/nlm/uts/clients/SourceDocumentationPreparation.java
+++ umls.nlm.nih.gov/UtsSourceDocumentation/src/gov/nih/nlm/uts/clients/SourceDocumentationPreparation.java
@@ -2,8 +2,6 @@ package gov.nih.nlm.uts.clients;
 
 import java.io.File;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
