2c4e6be88657c24b22028357e3052990037f2aa5
NIH\andersondm2 <david.anderson3@nih.gov>
2017-05-22 18:03:26 -0400

    Some jbake things - pom file, index template, scripts, css

---
+++ umls.nlm.nih.gov/sourcereleasedocs/scripts/replaceSourceTitle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script replaces the title field in the jbake metadata. It uses rsab-directory-map.csv to get from the 
+# directory name to the RSAB, which is inserted in the title field. 
+# run bash, navigate to the root directory (/sourcereleasedocs) and run scripts/replaceSourceTitle.sh
+# important - set the base directory to whichever directory contains SAB directories 
+BASEDIR='jbake/content/current/' 
+while IFS=',' read -r RSAB DIRECTORY; do 
+sed -i "/title=/c\title=$RSAB - Synopsis" "$BASEDIR""$DIRECTORY"/index.html;
+sed -i "/title=/c\title=$RSAB - Source Metadata" "$BASEDIR""$DIRECTORY"/Metadata.html;
+sed -i "/title=/c\title=$RSAB - Source Representation" "$BASEDIR""$DIRECTORY"/sourcerepresentation.html;
+sed -i "/title=/c\title=$RSAB - Metathesaurus Representation" "$BASEDIR""$DIRECTORY"/metarepresentation.html;
+done < rsab-directory-map.csv
