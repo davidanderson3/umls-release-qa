48cccd09cb27feecdd44935e9ae8b4d1e0a4107b
NIH\andersondm2 <david.anderson3@nih.gov>
2017-10-30 14:02:46 -0400

    2017AB updates, script updates, template updates

--- umls.nlm.nih.gov/sourcereleasedocs/scripts/replaceDate.sh
+++ umls.nlm.nih.gov/sourcereleasedocs/scripts/replaceDate.sh
@@ -2,11 +2,12 @@
 # run bash, navigate to the root directory (/sourcereleasedocs) and run bash scripts/replaceDate.sh
 # important - set the base directory to whichever directory contains SAB directories 
 # set a value for the CREATEDATE variable before running
-BASEDIR='jbake/content/current/'
-CREATEDATE='2017-05-08'
+BASEDIR='jbake/content/'
+CREATEDATE='2017-11-06'
 while IFS=',' read -r RSAB DIRECTORY; do
 sed -i "/date=/c\date=$CREATEDATE" "$BASEDIR""$DIRECTORY"/index.html;
-sed -i "/date=/c\date=$CREATEDATE" "$BASEDIR""$DIRECTORY"/Metadata.html;
+sed -i "/date=/c\date=$CREATEDATE" "$BASEDIR""$DIRECTORY"/metadata.html;
+sed -i "/date=/c\date=$CREATEDATE" "$BASEDIR""$DIRECTORY"/stats.html;
 sed -i "/date=/c\date=$CREATEDATE" "$BASEDIR""$DIRECTORY"/sourcerepresentation.html;
 sed -i "/date=/c\date=$CREATEDATE" "$BASEDIR""$DIRECTORY"/metarepresentation.html;
 done < rsab-directory-map.csv  
--- umls.nlm.nih.gov/sourcereleasedocs/scripts/replaceUpdatedDate.sh
+++ umls.nlm.nih.gov/sourcereleasedocs/scripts/replaceUpdatedDate.sh
@@ -2,11 +2,12 @@
 # run bash, navigate to the root directory (/sourcereleasedocs) and run bash scripts/replaceUpdatedDate.sh
 # important - set the base directory to whichever directory contains SAB directories 
 # set a value for the CREATEDATE variable before running
-BASEDIR='jbake/content/current/'
-UPDATEDATE='2017-05-08'
+BASEDIR='jbake/content/'
+UPDATEDATE='2017-11-06'
 while IFS=',' read -r RSAB DIRECTORY; do
 sed -i "/updated=/c\updated=$UPDATEDATE" "$BASEDIR""$DIRECTORY"/index.html;
-sed -i "/updated=/c\updated=$UPDATEDATE" "$BASEDIR""$DIRECTORY"/Metadata.html;
+sed -i "/updated=/c\updated=$UPDATEDATE" "$BASEDIR""$DIRECTORY"/metadata.html;
+sed -i "/updated=/c\updated=$UPDATEDATE" "$BASEDIR""$DIRECTORY"/stats.html;
 sed -i "/updated=/c\updated=$UPDATEDATE" "$BASEDIR""$DIRECTORY"/sourcerepresentation.html;
 sed -i "/updated=/c\updated=$UPDATEDATE" "$BASEDIR""$DIRECTORY"/metarepresentation.html;
 done < rsab-directory-map.csv 
