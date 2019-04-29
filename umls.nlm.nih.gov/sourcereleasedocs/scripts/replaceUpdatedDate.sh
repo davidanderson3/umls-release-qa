b3e10e5021cd2c4c6ce3af7e6304bfe1de195152
David Anderson <david.anderson3@nih.gov>
2019-04-29 13:52:14 -0400

    2019AA updates

--- umls.nlm.nih.gov/sourcereleasedocs/scripts/replaceUpdatedDate.sh
+++ umls.nlm.nih.gov/sourcereleasedocs/scripts/replaceUpdatedDate.sh
@@ -2,7 +2,7 @@
 # Updates the 'updated' date in the jbake metadata. Should match the latest release date. 
 # set a value for the UPDATEDATE variable before running
 # run from the base directory (.../sourcereleasedocs/)
-UPDATEDATE='2018-11-05'
+UPDATEDATE='2019-05-06'
 while IFS='|' read -r VCUI RCUI VSAB RSAB SON SF SVER VSTART VEND IMETA RMETA SLC SCC SRL TFR CFR CXTY TTYL ATNL LAT CENC CURVER SABIN SSN SCIT; do
 [ -f jbake/content/"$RSAB"/index.html ] && sed -i "/updated=/c\updated=$UPDATEDATE" jbake/content/"$RSAB"/index.html;
 [ -f jbake/content/"$RSAB"/metadata.html ] && sed -i "/updated=/c\updated=$UPDATEDATE" jbake/content/"$RSAB"/metadata.html;
