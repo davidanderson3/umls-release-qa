76f65d632da8bc60650ee98811a4b93913fba3f1
Anderson, David (NIH/NLM) [E] <david.anderson@nih.gov>
2021-04-23 14:41:39 -0400

    Updating MRSAB, dates, handling changed and new SABs

--- umls.nlm.nih.gov/sourcereleasedocs/scripts/replaceDate.sh
+++ umls.nlm.nih.gov/sourcereleasedocs/scripts/replaceDate.sh
@@ -2,7 +2,7 @@
 # It is probably not necessary to use this script for releases. It replaces the create date for files, and this is probably better done manually. Most files already have a create date, which was set to 2017-11-06 when we switched to JBake. 
 # set a value for the CREATEDATE variable before running. 
 # run from the base directory (.../sourcereleasedocs/)
-CREATEDATE='2020-05-04'
+CREATEDATE='2021-05-03'
 while IFS='|' read -r VCUI RCUI VSAB RSAB SON SF SVER VSTART VEND IMETA RMETA SLC SCC SRL TFR CFR CXTY TTYL ATNL LAT CENC CURVER SABIN SSN SCIT; do
 #sed -i "/date=/c\date=$CREATEDATE" jbake/content/"$RSAB"/index.html;
 #sed -i "/date=/c\date=$CREATEDATE" jbake/content/"$RSAB"/metadata.html;
--- umls.nlm.nih.gov/sourcereleasedocs/scripts/syncLatestVersion.sh
+++ umls.nlm.nih.gov/sourcereleasedocs/scripts/syncLatestVersion.sh
@@ -2,5 +2,5 @@
 # This script merges files from the version specified into the /jbake/content/ folder. 
 # Before running this, make sure the files in the versioned folder are finalized.
 # run from the base directory (.../sourcereleasedocs/)
-VERSION='2020AB'
+VERSION='2021AA'
 rsync -avh --progress "$VERSION"/ jbake/content/
