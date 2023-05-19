a9faa2fb86b6973700a89ef5fd75fa353fda6bdc
David Anderson <david.anderson@nih.gov>
2023-05-19 14:52:13 -0400

    NCI subsources

--- umls.nlm.nih.gov/sourcereleasedocs/scripts/archive.py
+++ umls.nlm.nih.gov/sourcereleasedocs/scripts/archive.py
@@ -2,49 +2,40 @@ import os
 from bs4 import BeautifulSoup
 
 # Function to replace the specified div in an HTML file
-def replace_div_in_html(file_path):
+def insert_div_after_endmenu(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
         soup = BeautifulSoup(content, 'html.parser')
-        divs = soup.find_all('div', class_='archived')
 
-        if len(divs) > 0:
-            for div in divs:
-                new_div = soup.new_tag('div')
-                new_div['class'] = 'new-div'
+        # Remove existing <div class="archived"> elements
+        for div in soup.find_all('div', class_='archived'):
+            div.decompose()
 
-                bold_tag = soup.new_tag('b')
-                bold_tag.string = 'This source is no longer available in the current UMLS release files. To get content from this source, download the '
+        new_div = '''
+        <div class="archived border border-danger p-3" style="background-color:#faf1f0;">
+            <b>This source is no longer available in the current version of the UMLS Metathesaurus. To obtain content from this source, download the <a href="https://www.nlm.nih.gov/research/umls/licensedcontent/umlsarchives04.html">2022AA UMLS release</a>. The content is also available in the <a href="https://www.nlm.nih.gov/research/umls/licensedcontent/umlsknowledgesources.html#umls-history-files">UMLS History Files</a>.</b>
+        </div>
+        '''
 
-                link1_tag = soup.new_tag('a')
-                link1_tag['href'] = 'https://www.nlm.nih.gov/research/umls/licensedcontent/umlsarchives04.html'
-                link1_tag.string = '2022AA UMLS release'
+        # Find the position of the <!--endmenu--> comment
+        end_menu_index = content.find('<!--endmenu-->')
 
-                link2_tag = soup.new_tag('a')
-                link2_tag['href'] = 'https://www.nlm.nih.gov/research/umls/licensedcontent/umlsknowledgesources.html'
-                link2_tag.string = 'UMLS History Files'
-
-                bold_tag.append(link1_tag)
-                bold_tag.append('. The content is also available in the ')
-                bold_tag.append(link2_tag)
-                bold_tag.append('.')
-
-                new_div.append(bold_tag)
-
-                # Replace the old div with the new div
-                div.replace_with(new_div)
+        if end_menu_index != -1:
+            # Insert the new div after the <!--endmenu--> comment
+            new_content = content[:end_menu_index + len('<!--endmenu-->')] + new_div + content[end_menu_index + len('<!--endmenu-->'):]
 
             # Save the modified HTML back to the file
             with open(file_path, 'w') as file:
-                file.write(str(soup))
+                file.write(new_content)
 
 # Directory where the HTML files are located
-root_directory = '/path/to/your/directory'
+root_directory = '/Users/andersondm2/umlsdoc/umls.nlm.nih.gov/sourcereleasedocs/jbake/content'
 
 # Iterate through all directories and files
 for root, dirs, files in os.walk(root_directory):
-    for file_name in files:
-        file_path = os.path.join(root, file_name)
+    if os.path.basename(root).startswith('NCI_'):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
 
-        if root.startswith('/path/to/your/directory/NCI_') and file_name.endswith('.html'):
-            replace_div_in_html(file_path)
+            if file_name.endswith('.html'):
+                insert_div_after_endmenu(file_path)
