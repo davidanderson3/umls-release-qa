883c076124fed0c62482036f346e4619c1d0c781
David Anderson <david.anderson@nih.gov>
2023-11-01 14:42:02 -0400

    updated script to generate home table for teamsite

--- umls.nlm.nih.gov/sourcereleasedocs/scripts/generateHome.sh
+++ umls.nlm.nih.gov/sourcereleasedocs/scripts/generateHome.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
-# Generate the vocab docs home page for pasting into Teamsite
-rm home_table.html
-echo "<p>Basic information about the source vocabularies represented in the UMLS.</p>
-<form id=\"form1\"></form>
-<table id=\"example\" class=\"display\" style=\"display:none;\" >" >> home_table.html
-echo "<thead>
-    <tr>
-      <th>Abbreviation</th>
-      <th>Name</th>
-      <th>Last Updated</th>
-      <th>Language</th>
-      <th>Restriction Level <a href="https://uts.nlm.nih.gov/uts/license/license-category-help.html" target="_blank">
-               (?)
-            </a></th>
-      
-    </tr>
-  </thead>" >> home_table.html
 
-CREATEDATE='2017-11-06'
+# Remove the old home_table.html if it exists
+rm -f home_table.html
+
+# Generate the initial part of the HTML table
+echo -e "<p>Basic information about the source vocabularies represented in the UMLS.</p>\n<form id=\"form1\"></form>\n<table id=\"example\" class=\"display\" style=\"display:none;\" >" > home_table.html
+
+# Generate the table header
+echo -e "<thead>\n<tr>\n<th>Abbreviation</th>\n<th>Name</th>\n<th>Last Updated</th>\n<th>Language</th>\n<th>Restriction Level <a href=\"https://uts.nlm.nih.gov/uts/license/license-category-help.html\" target=\"_blank\">(?)</a></th>\n</tr>\n</thead>" >> home_table.html
+
+# Read the MRSAB_all.txt file line by line and populate the table
 while IFS='|' read -r VCUI RCUI VSAB RSAB SON SF SVER VSTART VEND IMETA RMETA SLC SCC SRL TFR CFR CXTY TTYL ATNL LAT CENC CURVER SABIN SSN SCIT; do
-echo "<tr><td><a href=\"//www.nlm.nih.gov/research/umls/sourcereleasedocs/current/"$RSAB"\">$RSAB</a></td>" >> home_table.html;
-echo "<td>"$SSN"</td>" >> home_table.html;
-echo "<td>"$IMETA"</td>" >> home_table.html;
-echo "<td>"$LAT"</td>" >> home_table.html;
-echo "<td>"$SRL"</td></tr>" >> home_table.html;
+    # Include only rows where CURVER is "Y"
+    if [ "$CURVER" != "Y" ]; then
+        continue
+    fi
+
+    echo -e "<tr><td><a href=\"//www.nlm.nih.gov/research/umls/sourcereleasedocs/current/$RSAB\">$RSAB</a></td>\n<td>$SSN</td>\n<td>$IMETA</td>\n<td>$LAT</td>\n<td>$SRL</td></tr>" >> home_table.html
 done < MRSAB_all.txt
-echo "</table>" >> home_table.html
+
+# Close the table tag
+echo -e "</table>" >> home_table.html
