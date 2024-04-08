69b791bb82d93a5adfbee45b625dbe415be9cb3c
Anderson, David (NIH/NLM) [E] <david.anderson@nih.gov>
2024-04-08 10:10:10 -0400

    initializing angular project

---
+++ umls.nlm.nih.gov/vocabulary-documentation/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { SourcesComponent } from './sources/sources.component'
+import { HomePageComponent } from './home-page/home-page.component'
+
+
+const routes: Routes = [
+    // ... other routes
+    { path: '', component: HomePageComponent },
+    { path: 'current/:folder_name', component: SourcesComponent },
+    { path: 'current/:folder_name/index.html', component: SourcesComponent },
+    { path: 'current/:folder_name/metadata.html', component: SourcesComponent },
+    { path: 'current/:folder_name/stats.html', component: SourcesComponent },
+    { path: 'current/:folder_name/metarepresentation.html', component: SourcesComponent },
+    { path: 'current/:folder_name/sourcerepresentation.html', component: SourcesComponent },
+    // ... potentially other routes
+];
+
+@NgModule({
+	imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+	exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+RouterModule.forRoot(routes, { enableTracing: true }) // <-- debugging purposes only
