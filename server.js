00de8d7da4aae2ce697843e3e98790d6907855d6
davidanderson3 <dvdndrsn@gmail.com>
2025-07-02 16:22:54 -0400

    Remove compare line counts button

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const textsFile = path.join(__dirname, 'texts.json');
 const defaultTexts = {
   title: 'UMLS Release QA',
   header: 'UMLS Release QA',
-  runPreprocessButton: 'Run Reports',
-  compareLinesButton: 'Compare Line Counts'
+  runPreprocessButton: 'Run Reports'
 };
 
 function wrapHtml(title, body) {
