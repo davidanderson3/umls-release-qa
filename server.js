8cdc29fca3ecca6a8bf4378f413c40f075fe3d21
davidanderson3 <dvdndrsn@gmail.com>
2025-07-09 11:20:23 -0400

    Link MRDOC line counts to report

--- server.js
+++ server.js
@@ -308,6 +308,7 @@ app.get('/api/line-count-diff', async (req, res) => {
       else if (/^MRREL\.RRF$/i.test(base)) link = 'MRREL_report.html';
       else if (/^MRSAT\.RRF$/i.test(base)) link = 'MRSAT_report.html';
       else if (/^MRHIER\.RRF$/i.test(base)) link = 'MRHIER_report.html';
+      else if (/^MRDOC\.RRF$/i.test(base)) link = 'MRDOC_report.html';
       let status = 'n/a';
       if (link) {
         try {
